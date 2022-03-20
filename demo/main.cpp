2e8bb8e95a00a86d6c3dc6232ed2f5ab059e6b4b
kyzminyarik <Yarosav_kuzmin2002@mail.ru>
2022-03-20 01:29:50 -0700

    commit5

--- demo/main.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright 2022 kyzminyarik <Yaroslav_kuzmin2002@mail.ru>
-
-#include <Stack1.hpp>
-
-int main() {
-
-}
-
--- sources/source.cpp
+++
@@ -1,3 +0,0 @@
-// Copyright 2022 kyzminyarik <Yaroslav_kuzmin2002@mail.ru>
-
-#include <Stack1.hpp>
